8eab7fff30ee2cd93aa343338a8a282318795823
Pablo Henrique <125980185+PabloDevFE@users.noreply.github.com>
2023-07-05 09:19:32 -0300

    projeto finalizado

---
+++ geradorDeSenhas/script.js
@@ -0,0 +1,29 @@
+let sliderElement = document.querySelector("#slider")
+let buttonElement = document.querySelector("#button")
+
+let sizePassword = document.querySelector("#valor")
+let password = document.querySelector("#password")
+
+let containerPassword = document.querySelector("#containerPassword")
+let charset = "qwertyuiopasdfghjklçzxcvbnmQWERTYUIOPASDFGHJKLÇZXCVBNM1234567890/*-+,!@#$%&*()-=+_"
+let novaSenha = ""
+
+sizePassword.innerHTML = sliderElement.value
+
+slider.oninput = function (){
+    sizePassword.innerHTML = this.value
+}
+
+function generatePassword() {
+    let pass = ""
+    for(let i = 0, n = charset.length; i < sliderElement.value; i++) {
+        pass += charset.charAt(Math.floor(Math.random() * n))
+    }
+    containerPassword.classList.remove("hide")
+    password.innerHTML = pass
+    novaSenha = pass
+}
+
+function copyPass() {
+    navigator.clipboard.writeText(novaSenha)
+}
