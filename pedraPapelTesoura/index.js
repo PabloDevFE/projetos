035e005b79d12ce6d6afbdc28812c74102451c79
Pablo Henrique <125980185+PabloDevFE@users.noreply.github.com>
2023-07-24 12:01:27 -0300

    formatando o documento

--- pedraPapelTesoura/index.js
+++ pedraPapelTesoura/index.js
@@ -1,92 +1,83 @@
-const opcoes = document.querySelectorAll('.options')
-const exibir = document.querySelector('#resultAnteriorEmoji')
+const opcoes = document.querySelectorAll(".options");
+const exibir = document.querySelector("#resultAnteriorEmoji");
 
-let idOpcao
-let escolhaRandom 
-let resultado = document.querySelector('#resultado')
+let idOpcao;
+let escolhaRandom;
+let resultado = document.querySelector("#resultado");
 
-let selecionado = document.querySelector('#selecionado')
+let selecionado = document.querySelector("#selecionado");
 
-opcoes.forEach(opcao => {
-    opcao.addEventListener('mouseenter', () => {
-        //selecionar a opção e atribuir o valor a variavel idOpcao
-        const opcaoEscolhida = document.querySelector('.selecionado')
-        opcaoEscolhida.classList.remove('selecionado')
-        opcao.classList.add('selecionado')
-        idOpcao = opcao.attributes.id.value;
+opcoes.forEach((opcao) => {
+  opcao.addEventListener("mouseenter", () => {
+    //selecionar a opção e atribuir o valor a variavel idOpcao
+    const opcaoEscolhida = document.querySelector(".selecionado");
+    opcaoEscolhida.classList.remove("selecionado");
+    opcao.classList.add("selecionado");
+    idOpcao = opcao.attributes.id.value;
 
-        emoji()
-    })
-})
-
-
+    emoji();
+  });
+});
 
 function emoji() {
-    if (idOpcao == "tesoura") {
-        selecionado.innerHTML = "✌️"
-    } else if (idOpcao == "papel") {
-        selecionado.innerHTML = "🖐️"
-    } else if (idOpcao == "pedra") {
-        selecionado.innerHTML = "✊"
-    }
+  if (idOpcao == "tesoura") {
+    selecionado.innerHTML = "✌️";
+  } else if (idOpcao == "papel") {
+    selecionado.innerHTML = "🖐️";
+  } else if (idOpcao == "pedra") {
+    selecionado.innerHTML = "✊";
+  }
 }
 
 function randomize() {
-    const randomNumber = Math.floor(Math.random() * 9) + 1
-    if (randomNumber == 1 || randomNumber == 4 || randomNumber == 7) {
-        escolhaRandom = "papel"
-        selecionadoMaq.innerHTML = "🖐️"
-    } else if (randomNumber == 2 || randomNumber == 5 || randomNumber == 8) {
-        escolhaRandom = "tesoura"
-        selecionadoMaq.innerHTML = "✌️"
-    } else if (randomNumber == 3 || randomNumber == 6 || randomNumber == 9) {
-        escolhaRandom = "pedra"
-        selecionadoMaq.innerHTML = "✊"
-    }
+  const randomNumber = Math.floor(Math.random() * 9) + 1;
+  if (randomNumber == 1 || randomNumber == 4 || randomNumber == 7) {
+    escolhaRandom = "papel";
+    selecionadoMaq.innerHTML = "🖐️";
+  } else if (randomNumber == 2 || randomNumber == 5 || randomNumber == 8) {
+    escolhaRandom = "tesoura";
+    selecionadoMaq.innerHTML = "✌️";
+  } else if (randomNumber == 3 || randomNumber == 6 || randomNumber == 9) {
+    escolhaRandom = "pedra";
+    selecionadoMaq.innerHTML = "✊";
+  }
 }
 
 function result() {
-    //analisar o resultado
-    if (idOpcao == "pedra" && escolhaRandom == "papel") {
-        resultado.innerText = "Você perdeu!"
-    } else if (idOpcao == "papel" && escolhaRandom == "tesoura") {
-        resultado.innerText = "Você perdeu!"
-    } else if (idOpcao == "tesoura" && escolhaRandom == "pedra") {
-        resultado.innerText = "Você perdeu!"
-    } else if (idOpcao == "pedra" && escolhaRandom == "tesoura") {
-        resultado.innerText = "Você ganhou!"
-    } else if (idOpcao == "tesoura" && escolhaRandom == "papel") {
-        resultado.innerText = "Você ganhou!"
-    } else if (idOpcao == "papel" && escolhaRandom == "pedra") {
-        resultado.innerText = "Você ganhou!"
-    } else {
-        resultado.innerText = "Empate!"
-    }
+  //analisar o resultado
+  if (idOpcao == "pedra" && escolhaRandom == "papel") {
+    resultado.innerText = "Você perdeu!";
+  } else if (idOpcao == "papel" && escolhaRandom == "tesoura") {
+    resultado.innerText = "Você perdeu!";
+  } else if (idOpcao == "tesoura" && escolhaRandom == "pedra") {
+    resultado.innerText = "Você perdeu!";
+  } else if (idOpcao == "pedra" && escolhaRandom == "tesoura") {
+    resultado.innerText = "Você ganhou!";
+  } else if (idOpcao == "tesoura" && escolhaRandom == "papel") {
+    resultado.innerText = "Você ganhou!";
+  } else if (idOpcao == "papel" && escolhaRandom == "pedra") {
+    resultado.innerText = "Você ganhou!";
+  } else {
+    resultado.innerText = "Empate!";
+  }
 
-
-    //colocar o resultado e a jogada 
-    exibir.innerHTML = selecionado.innerHTML
-
-
-
-    //limpar o resultado
+  //colocar o resultado e a jogada
+  exibir.innerHTML = selecionado.innerHTML;
 }
 
 function esconder() {
-    let divResultAnterior = document.querySelector("#resultAnterior")
-    divResultAnterior.classList.add('hide')
+  let divResultAnterior = document.querySelector("#resultAnterior");
+  divResultAnterior.classList.add("hide");
 }
 
 function play() {
-    randomize()
+  randomize();
 
-    result()
+  result();
 
-    let divResultAnterior = document.querySelector("#resultAnterior")
-    divResultAnterior.classList.remove('hide')
+  let divResultAnterior = document.querySelector("#resultAnterior");
+  divResultAnterior.classList.remove("hide");
 
-
-
-    console.log(idOpcao)
-    console.log(escolhaRandom)
+  console.log(idOpcao);
+  console.log(escolhaRandom);
 }
