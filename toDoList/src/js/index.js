b8058312b6b12cb0a15dfeb71598cfd1693099ca
Pablo Henrique <125980185+PabloDevFE@users.noreply.github.com>
2023-08-23 10:20:35 -0300

    atualização
    corrigindo bug das fonts, bastou adicionar o format. Também comecei a criar o script para adicionar os elementos

--- toDoList/src/js/index.js
+++ toDoList/src/js/index.js
@@ -1,10 +1,36 @@
-const botao = document.querySelector("#botaoAdicionar")
-const listaImportante = document.querySelector("#listaImportante")
+const botao = document.querySelector("#botaoAdicionar");
 
-let criarLi = document.createElement("li")
+const listaImportante = document.querySelector("#listaImportante");
+const listaNormal = document.querySelector("#listaNormal")
+
+const textInput = document.querySelector("#toDoText");
+
+const checkNormal = document.querySelector("#checkImportante")
+const checkImportante = document.querySelector("#checkNormal")
+
+
+let numeroId = 1
 
 botao.addEventListener('click', () => {
-    criarLi.innerText = "novo li"
-    console.log("clicou")
-    listaImportante.appendChild(criarLi)
-})
+  let tarefa = textInput.value;
+
+  console.log(checkImportante.checked)
+  console.log(checkNormal.checked)
+
+  let li = `<li>
+            <label for="toDoItem${numeroId}">
+                <input type="checkbox" name="" id="toDoItem${numeroId}" /> ${tarefa} 
+            </label>
+            <span class="iconExcluir" id="toDoExcluir${numeroId}">X</span>
+            </li>`;
+
+
+    listaImportante.innerHTML += li
+
+
+//Incrementar +1 no id
+    numeroId++
+
+//limpar o input
+    textInput.value = ""
+});
